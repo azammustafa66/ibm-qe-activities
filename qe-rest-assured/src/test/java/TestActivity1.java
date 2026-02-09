4fd3fb098d38f86e7058384edf0d8cb853b7f270
Azam <azammustafa66@gmail.com>
2026-02-09 12:26:29 +0530

    Add initial test classes for Activity1, Activity2, and Activity3

---
+++ qe-rest-assured/src/test/java/TestActivity1.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Activity1 {
+    private String baseURL = "petstore.swagger.io/v2/pet/";
+
+    @Test
+    public void postRequest() {
+        HashMap<String, Object> reqBody = new HashMap<>();
+        
+    }
+}
---
+++ qe-rest-assured/src/test/java/TestActivity2.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class TestActivity2 {
+}
